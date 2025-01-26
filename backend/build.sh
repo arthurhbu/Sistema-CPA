902b14c03c3d3175a302f20b77260bd6c7f2608b
Dioclecio Camelo <dioclecio@hotmail.com>
2025-01-26 12:51:22 -0300

    Montagem dos Builds e ajustes para o repositório do Quay.io

---
+++ backend/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+version="v$(date +'%Y%m%d')"
+# Limpa os repositórios locais
+podman rmi sistema-backend:$version
+podman rmi sistema-backend:latest
+podman build . -t sistema-backend:$version -t sistema-backend:latest
+
+# Faz o login para enviar aos repositórios públicos
+podman login quay.io --authfile ../auth.json
+
+podman push sistema-backend:$version quay.io/uemcpa/sistema-backend:$version
+podman push sistema-backend:latest quay.io/uemcpa/sistema-backend:latest
---
+++ frontend/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+version="v$(date +'%Y%m%d')"
+# Limpa os repositórios locais
+podman rmi sistema-frontend:$version
+podman rmi sistema-frontend:latest
+podman build . -t sistema-frontend:$version -t sistema-frontend:latest
+
+# Faz o login para enviar aos repositórios públicos
+podman login quay.io --authfile ../auth.json
+
+podman push sistema-frontend:$version quay.io/uemcpa/sistema-frontend:$version
+podman push sistema-frontend:latest quay.io/uemcpa/sistema-frontend:latest
